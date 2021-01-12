6968e063d8738b3ae4cf18482c4004cd66f603ae
Richard Walter <richardwalter515@gmail.com>
2021-01-12 18:14:32 -0500

    updated folder structure and linked html routes

---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+
+// routes
+app.get("/api/workouts", (req, res) => {
+    Workout.create({}).then(dbWorkout => {res.json(dbWorkout)})
+    //catching errors if it's 500, printing in JSON
+    .catch(err => res.status(500).json(err))
+});
+
+//post note
+
+app.post("/notes", function(req, res) {
+  store
+  .addNotes(req.body)
+  .then((note) => res.json(note))
+  .catch(err => res.status(500).json(err))
+});
+
+//delete note
+app.delete("/notes/:id", function(req, res) {
+  const id = req.params.id;
+  store
+  .removeNotes(id)
+  .then((note) => res.json(note))
+  .catch(err => {
+    console.log("error", err);
+    res.status(500).json(err)
+  }
+)});
+
+module.exports = app;
--- Develop/server.js
+++ Develop/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const apiroutes = require("./routes/apiRoutes");
+const htmlroutes = require("./routes/htmlRoutes");
 
 const PORT = process.env.PORT || 3000;
 
-const User = require("./userModel.js");
+const User = require("./models/userModel.js");
 
 const app = express();
 
@@ -17,19 +19,8 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/custommethods", { useNewUrlParser: true });
 
-app.post("/submit", ({ body }, res) => {
-  const user = new User(body);
-  user.coolifier();
-  user.makeCool();
-
-  User.create(user)
-    .then(dbUser => {
-      res.json(dbUser);
-    })
-    .catch(err => {
-      res.json(err);
-    });
-});
+app.use("/api", apiroutes);
+app.use("/", htmlroutes);
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
